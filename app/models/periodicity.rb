7f6474b53884d284d6c2c725831bfc4910352e2f
Giridhar K <giridhar@blububirch>
2015-06-16 10:42:40 +0530

    Added end_date funtion.

--- app/models/periodicity.rb
+++ app/models/periodicity.rb
@@ -6,4 +6,26 @@ class Periodicity < ActiveRecord::Base
 
   validates :name, presence: :true
   validates :rank, presence: :true
+
+  def end_date(start_date)
+    if :name == 'Monthly'
+      start_date + 1.month - 1
+    elsif :name == 'One-Time'
+      start_date
+    elsif :name == 'Weekly'
+      start_date + 1.week - 1
+    elsif :name == 'Annually'
+      start_date + 1.year - 1
+    elsif :name == 'Daily'
+      start_date + 1.day - 1
+    elsif :name == 'Quarterly'
+      start_date + 3.month - 1
+    elsif :name == 'Fortnightly'
+      start_date + 2.week - 1
+    elsif :name == 'Semi-Annually'
+      start_date + 6.month - 1
+    else
+      start_date
+    end
+  end
 end
