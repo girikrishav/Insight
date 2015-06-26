e941620f944c2cf35c1c7aa89662f6ed2bd823b6
Giridhar K <giridhar@blububirch>
2015-06-26 18:52:37 +0530

    [Intermediate] Added invoice_headers model only.

--- app/models/invoice_header.rb
+++ app/models/invoice_header.rb
@@ -4,6 +4,8 @@ class InvoiceHeader < ActiveRecord::Base
   validates :invoice_status_id, presence: :true
   validates :term_id, presence: :true
 
+  # Dummy
+
   belongs_to :project, :class_name => 'Project', :foreign_key => :project_id
   belongs_to :invoice_status, :class_name => 'InvoiceStatus', :foreign_key => :invoice_status_id
   belongs_to :term, :class_name => 'Term', :foreign_key => :term_id
