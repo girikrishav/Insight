5e439a26de4e59a31af9c59dd7a8c8842520b00a
girikrishav <girikrishva@gmail.com>
2015-09-17 09:37:14 +0530

    Bug in validates clause.

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,7 +1,7 @@
 class Holiday < ActiveRecord::Base
   validates :name, presence: :true
   validates :as_on, presence: :true
-  validates :business_units, presence: :true
+  validates :business_unit, presence: :true
 
   validates_uniqueness_of :name, scope: [:name, :as_on, :business_unit_id]
 
